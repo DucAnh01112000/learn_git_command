a0889e9c819908582dcce19d5d2e0aa9c725f5b9
DucAnh01112000 <01112000hda@gmail.com>
2024-07-07 22:23:25 +0700

    commit 6 for main branch

--- commands_git/src/main/java/git_rebase/main_branch.java
+++ commands_git/src/main/java/git_rebase/main_branch.java
@@ -2,6 +2,7 @@ package git_rebase;
 
 public class main_branch {
     System.out.println("commit 1")
-    string a = "commit 3 of main branch";
-    string b = "commit 4 for main branch";
+    String a = "commit 3 of main branch";
+    String b = "commit 4 for main branch";
+    String c = "commit 6 for main branch";
 }
