2884a063fe991fe217438048eb1753bfc55b63cf
DucAnh01112000 <01112000hda@gmail.com>
2024-07-09 23:17:38 +0700

    name and age
    dz vailong

---
+++ commands_git/src/main/java/git_rebase/ducanh_dz.java
@@ -0,0 +1,13 @@
+package git_rebase;
+
+public class ducanh_dz {
+    String person_name = "ducanh";
+    String age = "24";
+
+    int salary = 7;
+
+    int how_far_to_work = 10;
+
+    jfaklsjdlfjlaksjdf
+
+}
